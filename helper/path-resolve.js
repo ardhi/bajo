999e323365e2943b21240beafe44b3ad892a81d3
Ardhi Lukianto <ardhi@lukianto.com>
2023-06-21 20:56:36 +0700

    Handle file url

--- helper/path-resolve.js
+++ helper/path-resolve.js
@@ -3,6 +3,7 @@ import os from 'os'
 
 export default {
   handler: function (item, asFileUrl) {
+    if (item.startsWith('file://')) item = item.slice(item[7] !== '/' ? 7 : 8)
     item = path.resolve(item)
     if (os.platform() === 'win32') {
       item = item.replace(/\\/g, '/')
