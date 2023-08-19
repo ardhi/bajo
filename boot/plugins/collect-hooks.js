25dfa4f48f44ff9f74a2557400bd266f3b2f2d52
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-19 09:57:40 +0700

    Sub NS

--- boot/plugins/collect-hooks.js
+++ boot/plugins/collect-hooks.js
@@ -1,17 +1,19 @@
 import { map, camelCase, merge, filter, groupBy } from 'lodash-es'
 
 async function collectHooks () {
-  const { eachPlugins, log, runHook, isLogInRange, importModule } = this.bajo.helper
+  const { eachPlugins, log, runHook, isLogInRange, importModule, pascalCase } = this.bajo.helper
   this.bajo.hooks = this.bajo.hooks ?? []
   log.debug('Collect hooks')
   // collects
   await eachPlugins(async function ({ plugin, dir, file }) {
     const hookName = (file.slice(dir.length + 1) ?? '').split('/')[1]
-    let [ns, path] = map(hookName.replace('.js', '').split('@'), e => camelCase(e))
+    let [ns, path] = hookName.replace('.js', '').split('@')
     if (!path) {
       path = ns
       ns = plugin
     }
+    path = camelCase(path)
+    ns = map(ns.split('.'), (n, i) => i === 1 ? pascalCase(n) : camelCase(n)).join('.')
     const mod = await importModule(file, { asHandler: true })
     if (!mod) return undefined
     merge(mod, { ns, path })
@@ -21,7 +23,7 @@ async function collectHooks () {
   // for log trace purpose only
   if (!isLogInRange('trace')) return
   await eachPlugins(async function ({ plugin }) {
-    const hooks = filter(this.bajo.hooks, { ns: plugin })
+    const hooks = filter(this.bajo.hooks, h => h.ns.startsWith(plugin))
     if (hooks.length === 0) return undefined
     const items = groupBy(hooks, 'path')
     for (const hook of hooks) {
