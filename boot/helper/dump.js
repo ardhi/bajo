1825a173a9b271ad6286dc95f5a764b3e0926277
Ardhi Lukianto <ardhi@lukianto.com>
2024-04-20 11:40:28 +0700

    Updates

--- boot/helper/dump.js
+++ boot/helper/dump.js
@@ -1,10 +1,14 @@
 import util from 'util'
+import { last } from 'lodash-es'
 
 const dump = (...args) => {
+  const terminate = last(args) === true
+  if (terminate) args.pop()
   for (const arg of args) {
     const result = util.inspect(arg, false, null, true)
     console.log(result)
   }
+  if (terminate) process.kill(process.pid, 'SIGINT')
 }
 
 export default dump
