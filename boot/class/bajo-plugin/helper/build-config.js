def9d95b7faae0b5f64c680428c0b837c80ff01b
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-20 21:12:13 +0700

    Rename method: plugin.readConfig -> plugin.loadConfig

--- boot/class/bajo-plugin/helper/build-config.js
+++ boot/class/bajo-plugin/helper/build-config.js
@@ -3,7 +3,7 @@ import { camelCase } from 'lodash-es'
 async function buildConfig () {
   this.bajo.log.debug('Read configurations')
   for (const pkg of this.bajo.pluginPkgs) {
-    await this[camelCase(pkg)].readConfig()
+    await this[camelCase(pkg)].loadConfig()
   }
 }
 
