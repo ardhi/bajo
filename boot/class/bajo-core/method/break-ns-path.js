535b30c96901f6c47765476ab02460b6e82b37bc
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-27 07:57:59 +0700

    Add subNs

--- boot/class/bajo-core/method/break-ns-path.js
+++ boot/class/bajo-core/method/break-ns-path.js
@@ -2,18 +2,19 @@ import { isEmpty } from 'lodash-es'
 
 function breakNsPath (item = '', defaultNs = 'bajo') {
   let [ns, ...path] = item.split(':')
+  let subNs
   path = path.join(':')
   if (isEmpty(path)) {
     path = ns
     ns = defaultNs
   }
-  if (ns.length === 1) return [ns, path].join(':') // windows fs
+  [ns, subNs] = ns.split('.')
   if (!this.app[ns]) {
     const plugin = this.getPlugin(ns)
     if (plugin) ns = plugin.name
   }
   if (!this.app[ns]) throw this.error('Unknown plugin \'%s\' or plugin isn\'t loaded yet')
-  return [ns, path]
+  return [ns, path, subNs]
 }
 
 export default breakNsPath
