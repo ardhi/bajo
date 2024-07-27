27c84f95a3124e009d09687679ece2c2ec9fa50b
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-27 07:58:14 +0700

    Handle win fs

--- boot/class/bajo-core/method/get-plugin-file.js
+++ boot/class/bajo-core/method/get-plugin-file.js
@@ -3,6 +3,7 @@ import breakNsPath from './break-ns-path.js'
 function getPluginFile (file) {
   if (!this) return file
   if (file.includes(':')) {
+    if (file.slice(1, 2) === ':') return file // windows fs
     const [ns, path] = breakNsPath.call(this, file)
     if (ns !== 'file' && this && this.app && this.app[ns] && ns.length > 1) {
       file = `${this.app[ns].config.dir.pkg}${path}`
