c098f5e3a5edc713190683eab685cd3475ec0440
Ardhi Lukianto <ardhi@lukianto.com>
2024-08-09 09:48:19 +0700

    Read default, then env specific

--- boot/lib/read-all-configs.js
+++ boot/lib/read-all-configs.js
@@ -1,7 +1,23 @@
 import readConfig from '../class/bajo-core/method/read-config.js'
+import defaultsDeep from '../class/bajo-core/method/defaults-deep.js'
 
 async function readAllConfigs (base) {
   let cfg = {}
+  let ext = {}
+  // default config file
+  try {
+    cfg = await readConfig.call(this.bajo, `${base}.*`)
+  } catch (err) {
+    if (['BAJO_CONFIG_NO_PARSER'].includes(err.code)) throw err
+  }
+  // env based config file
+  try {
+    ext = await readConfig.call(this.bajo, `${base}-${this.bajo.config.env}.*`)
+  } catch (err) {
+    if (!['BAJO_CONFIG_FILE_NOT_FOUND'].includes(err.code)) throw err
+  }
+  return defaultsDeep({}, ext, cfg)
+  /*
   try {
     cfg = await readConfig.call(this.bajo, `${base}-${this.bajo.config.env}.*`)
   } catch (err) {
@@ -14,7 +30,7 @@ async function readAllConfigs (base) {
       }
     }
   }
-  return cfg
+  */
 }
 
 export default readAllConfigs
